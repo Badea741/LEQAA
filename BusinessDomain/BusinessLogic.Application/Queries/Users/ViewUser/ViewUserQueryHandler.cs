ce8115df456c756f1d7e62790be1fc3530d5e51b
badea741 <mohammedahmedbadee@gmail.com>
2023-03-13 00:37:23 +0200

    Merged new changes with business

--- BusinessDomain/BusinessLogic.Application/Queries/Users/ViewUser/ViewUserQueryHandler.cs
+++ BusinessDomain/BusinessLogic.Application/Queries/Users/ViewUser/ViewUserQueryHandler.cs
@@ -16,7 +16,7 @@ public class ViewUserQueryHandler : IHandler<ViewUserQuery, ErrorOr<UserReadMode
 
     public async Task<ErrorOr<UserReadModel>> Handle(ViewUserQuery request, CancellationToken cancellationToken)
     {
-        var user = (await _userRepository.GetAsync(u => u.UserName == request.UserName, null!, "Plans,Hubs,Channels,HubAnnouncements,ChannelAnnouncements")).FirstOrDefault()!;
+        var user = (await _userRepository.GetAsync(u => u.UserName == request.UserName, null!, "Plans,Posts,Hubs,Channels,HubAnnouncements,ChannelAnnouncements")).FirstOrDefault()!;
         return user.Adapt<UserReadModel>();
     }
 }
