2d734c4644a070c4ba806bd5a2a9526f6b2ce925
badea741 <mohammedahmedbadee@gmail.com>
2023-03-14 14:20:22 +0200

    Merged new changes from business

--- BusinessDomain/BusinessLogic.Application/Commands/Users/LeaveChannel/LeaveChannelCommand.cs
+++ BusinessDomain/BusinessLogic.Application/Commands/Users/LeaveChannel/LeaveChannelCommand.cs
@@ -1,6 +1,6 @@
 ﻿using BusinessLogic.Application.CommandInterfaces;
 using BusinessLogic.Application.Models.Channels;
-using BusinessLogic.Application.Models.Users;
+using BusinessLogic.Application.Models.Hubs;
 using ErrorOr;
 using System;
 using System.Collections.Generic;
@@ -8,11 +8,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BusinessLogic.Application.Queries.Hubs.viewHubUsers
+namespace BusinessLogic.Application.Commands.Users.LeaveChannel
 {
-    public record ViewHupUsersQuery
-   (
-        Guid hubid
-
-       ) : IQuery<ErrorOr<List<UserRecentReadModel>>>;
+    public record LeaveChannelCommand(
+     string UserName,
+     Guid ChannelId
+ ) : ICommand<ErrorOr<ChannelReadModel>>;
 }
